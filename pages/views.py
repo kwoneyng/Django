b01115d40d0d6ca0e6f7eb6f01f88f4bd33f8331
박권응 <nan308@naver.com>
2019-08-14 16:25:43 +0900

    Learn variable routing

--- pages/views.py
+++ pages/views.py
@@ -10,10 +10,35 @@ def introduce(request):
     return render(request, 'introduce.html')
 
 
-def dinner(request):
+def dinner(request, name):
     menu = ['강남 더막창스', '노랑통닭', '양자강']
     pick = random.choice(menu)
     context = {
         'pick':pick,
+        'name':name,
     }
     return render(request, 'dinner.html', context)
+
+
+def image(request):
+    image_url = 'https://picsum.photos/700'
+    context = {
+        'img_url':image_url,
+    }
+    return render(request, 'image.html', context)
+
+
+def greeting(request, name):
+    context = {
+        'name':name,
+    }
+    return render(request, 'greeting.html', context)
+
+
+def times(request, num1, num2):
+    context = {
+        'num1':num1,
+        'num2':num2,
+        'result':num1*num2,
+    }
+    return render(request, 'times.html', context)
